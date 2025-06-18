991bcc468052094126918219086a955ce1f0db63
Navneet Rai <navneet.rai@smartcomputerlabs.com>
2025-06-18 12:23:47 +0530

    changes for clamav set up

--- src/main/java/org/javacommunity/scan/controller/VirusScanController.java
+++ src/main/java/org/javacommunity/scan/controller/VirusScanController.java
@@ -12,6 +12,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.List;
 
 @RestController
 public class VirusScanController {
@@ -24,25 +25,32 @@ public class VirusScanController {
 
     @PostMapping(path = "/scan", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     @ResponseBody
-    public String handleFileScan(@RequestPart MultipartFile file) {
-        System.out.println(" file type -- "+file.getContentType());
-        System.out.println(" file type -- "+file.getSize());
-        if(file.isEmpty()) {
-            return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"No File Attached Or Invalid File\"}";
-        } else if (!file.getContentType().equals("application/pdf")) {
-            //Files.probeContentType();
-            return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"Only PDF file is Allowed\"}";
-        } else if (file.getSize() > 10000000l) {//in bytes
-            //Files.probeContentType();
-            return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"File Size Exceeds Maximum Allowed Size\"}";
-        }
-        try {
-            InputStream inputStream = file.getInputStream();
-            System.out.println("Start scan for - "+file.getOriginalFilename());
-            return virusScanService.isClean(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"Malformed File\"}";
+    public String handleFileScan(@RequestPart List<MultipartFile> attachment) {
+        if (attachment.size() > 1) {
+            System.out.println(" number of files attached -- " + attachment.size());
+            return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"Only One File Allowed\"}";
+        } else {
+            MultipartFile file = attachment.get(0);
+            System.out.println(" file type -- " + file.getContentType());
+            System.out.println(" file type -- " + file.getSize());
+            if (file.isEmpty()) {
+                return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"No File Attached Or Invalid File\"}";
+            } else if (!file.getContentType().equals("application/pdf")) {
+                //Files.probeContentType();
+                return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"Only PDF file is Allowed\"}";
+            } else if (file.getSize() > 10000000l) {//in bytes
+                //Files.probeContentType();
+                return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"File Size Exceeds Maximum Allowed Size\"}";
+            }
+            try {
+                InputStream inputStream = file.getInputStream();
+                System.out.println("Start scan for - " + file.getOriginalFilename());
+                return virusScanService.isClean(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "{\"FILE_STATUS\":\"INFECTED\",\"MESSAGE\":\"Malformed File\"}";
+            }
         }
     }
+
 }
